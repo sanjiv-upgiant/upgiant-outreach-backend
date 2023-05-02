074f6387b5bfa620eaaaf9c064a080eae0630e28
sanjiv <sanjivgautamofficial@gmail.com>
2023-05-02 20:16:30 +0545

    adds puppeteer chromium browser

--- src/crawler/scraper.ts
+++ src/crawler/scraper.ts
@@ -1,11 +1,18 @@
+import { PuppeteerLaunchOptions } from 'puppeteer';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
 puppeteer.use(StealthPlugin());
 
+const options: PuppeteerLaunchOptions = {};
 
 const scrape = async (url: string) => {
-    const browser = await puppeteer.launch();
+    const env = process.env["NODE_ENV"] || 'development';
+    if (env !== "development") {
+        console.log("loading prod environment");
+        options.executablePath = "/usr/bin/chromium-browser"
+    }
+    const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
     await page.goto(url);
 
