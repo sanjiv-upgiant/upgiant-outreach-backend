1d27ed270027ff1bbc3022bbd8ae2b504c81f145
sanjiv <sanjivgautamofficial@gmail.com>
2023-05-07 14:26:41 +0545

    adds new email template for writing email

--- src/modules/langchain/email/templates/email.template.ts
+++ src/modules/langchain/email/templates/email.template.ts
@@ -26,11 +26,11 @@ export const humanPromptTemplateStringForInitialOutput = `Here's the details of 
     Include following Details for email:
     {includeDetails}
 
-    Do not use exclamation. Write email in a professional manner. No signature in email body please.
+    Do not use exclamation. Write email in a professional manner. No signature in email body please. Utilize customer information properly.
 
     Output should contains following two fields. 
     subject: subject of the email.
-    body: Body of the email.`;
+    body: Body of the email without email sign offs`;
 
 export const systemPromptTemplateStringForFinalOutput = `You are an AI agent that works with email. You will receive a raw email subject and email body. You should edit email messages and delete/remove unwanted parts. Rephrase placeholder texts like [Your Name]. For EmailSignoffs like "Best regards" "Sincerely" "cheers". Delete everything including and after the EmailSignoff. If you have to rephrase the sentence, do it. Remember: You only respond in JSON and it should contain 2 fields.  
 
