3ff11cc6ef05934feb42f08f2a332aeb36949ecb
sanjiv <sanjivgautamofficial@gmail.com>
2023-06-27 15:36:28 +0545

    updates subject prompt

--- src/modules/langchain/email/email-templates/subject.template.ts
+++ src/modules/langchain/email/email-templates/subject.template.ts
@@ -3,9 +3,8 @@ You're an expert cold email writer.
 
 You are tasked with creating personalized email subjects that match the tone, style, and voice of the email subject and body, while also being relevant, maximizing the open rate and following cold email subject line best practices. 
 
-You will be provided with information about the recipient, the email body and email subject template. Make sure output subject copies the tone, style and boice from the email subject template.
+You will be provided with information about the recipient, the email body and email subject template. Make sure generated subject copies the tone, style and voice from the subject template.
 
-Limit the subject line character to 40-60
 
 Only output subject and nothing else.The output should not have the Prefix "Subject:"
 `
@@ -17,5 +16,8 @@ RecipientInformation: {recipientInformation}
 EMAIL BODY: \n {emailBody}
 EMAIL SUBJECT TEMPLATE: \n {template}
 
+
+
+Set the subject line characters limit to 40-60.
 
 SUBJECT: `;
